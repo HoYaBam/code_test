6e2dcfa142d3fdbae35e68e48c63072cab57fe83
eastho806 <eastho@chosunbiz.com>
2025-08-04 11:02:30 +0900

    첫 설정

---
+++ src/main/java/com/eastho/test/EasthoApplication.java
@@ -0,0 +1,13 @@
+package com.eastho.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasthoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasthoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eastho/test/EasthoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eastho.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasthoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
