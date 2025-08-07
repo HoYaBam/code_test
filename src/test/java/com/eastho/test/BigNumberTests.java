a2c234f210149decf93cb7e0276a73027c6dd807
eastho806 <eastho@chosunbiz.com>
2025-08-07 17:25:34 +0900

    추가

---
+++ src/test/java/com/eastho/test/BigNumberTests.java
@@ -0,0 +1,72 @@
+package com.eastho.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+@SpringBootTest
+class BigNumberTests {
+    /*
+    https://school.programmers.co.kr/learn/courses/30/lessons/42746
+    0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요.
+
+    예를 들어, 주어진 정수가 [6, 10, 2]라면 [6102, 6210, 1062, 1026, 2610, 2106]를 만들 수 있고, 이중 가장 큰 수는 6210입니다.
+
+    0 또는 양의 정수가 담긴 배열 numbers가 매개변수로 주어질 때, 순서를 재배치하여 만들 수 있는 가장 큰 수를 문자열로 바꾸어 return 하도록 solution 함수를 작성해주세요.
+
+    제한 사항
+    numbers의 길이는 1 이상 100,000 이하입니다.
+    numbers의 원소는 0 이상 1,000 이하입니다.
+    정답이 너무 클 수 있으니 문자열로 바꾸어 return 합니다.
+    입출력 예
+    numbers	return
+    [6, 10, 2]	"6210"
+    [3, 30, 34, 5, 9]	"9534330"
+
+     */
+    @Test
+    void contextLoads() {
+        int[] numbers = {6, 10, 2, 9, 20, 30, 44, 20, 97, 23, 56};
+
+        System.out.println(permutation(numbers));
+    }
+
+    public static String permutation(int[] numbers) {
+        String[] arr = Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .toArray(String[]::new);
+
+        System.out.println(Arrays.toString(arr));
+
+        Arrays.sort(arr, (a, b) -> (b + a).compareTo(a + b));
+
+
+        if (arr[0].equals("0")) return "0";
+
+        return String.join("", arr);
+    }
+
+
+//    private void permutation(int[] numbers, int cur) {
+//        if (cur == numbers.length) {
+//            StringBuilder sb = new StringBuilder();
+//            for (int number : numbers) sb.append(number);
+//            if (new BigInteger(sb.toString()).compareTo(new BigInteger(max)) > 0) max = sb.toString();
+//            return;
+//        }
+//
+//        for (int i = cur; i < numbers.length; i++) {
+//            swap(numbers, cur, i);
+//            permutation(numbers, cur + 1);
+//            swap(numbers, cur, i);
+//        }
+//    }
+//
+//    private void swap(int[] arr, int i, int j) {
+//        int tmp = arr[i];
+//        arr[i] = arr[j];
+//        arr[j] = tmp;
+//    }
+}
---
+++ src/test/java/com/eastho/test/InitialValueMaximumValueTests.java
@@ -0,0 +1,54 @@
+package com.eastho.test;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.*;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+class InitialValueMaximumValueTests {
+    private static final Logger log = LoggerFactory.getLogger(InitialValueMaximumValueTests.class);
+
+    /*
+        https://school.programmers.co.kr/learn/courses/30/lessons/12939
+        문자열 s에는 공백으로 구분된 숫자들이 저장되어 있습니다. str에 나타나는 숫자 중 최소값과 최대값을 찾아 이를 "(최소값) (최대값)"형태의 문자열을 반환하는 함수, solution을 완성하세요.
+        예를들어 s가 "1 2 3 4"라면 "1 4"를 리턴하고, "-1 -2 -3 -4"라면 "-4 -1"을 리턴하면 됩니다.
+
+        제한 조건
+        s에는 둘 이상의 정수가 공백으로 구분되어 있습니다.
+         */
+    @Test
+    void contextLoads() {
+        String s = "-3 -2 -4 -5"; //	"1 4"
+        String answer = "";
+        String[] tokens = s.split(" ");
+        IntStream.Builder negBuilder = IntStream.builder();
+        IntStream.Builder posBuilder = IntStream.builder();
+
+        for (String token : tokens) {
+            int num = Integer.parseInt(token);
+            if (num < 0) {
+                negBuilder.add(num);
+            } else if (num > 0) {
+                posBuilder.add(num);
+            }
+        }
+
+        int[] neg = negBuilder.build().toArray();
+        int[] pos = posBuilder.build().toArray();
+
+        if (pos.length == 0) {
+            answer = Arrays.stream(neg).min().getAsInt() + " " + Arrays.stream(neg).max().getAsInt();
+        } else if (neg.length == 0) {
+            answer = Arrays.stream(pos).min().getAsInt() + " " + Arrays.stream(pos).max().getAsInt();
+        } else {
+            answer = Arrays.stream(neg).min().getAsInt() + " " + Arrays.stream(pos).max().getAsInt();
+        }
+
+        System.out.println(answer);
+    }
+}
