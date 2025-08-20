519aa24b5b2d5f6356c26bf5dea6695b51431c0f
eastho806 <eastho@chosunbiz.com>
2025-08-20 17:55:03 +0900

    불필요 코드 제거

--- src/main/java/com/hoyabam/codeTest/CT87946.java
+++ src/main/java/com/hoyabam/codeTest/CT87946.java
@@ -45,7 +45,6 @@ public class CT87946 {
     public static void main(String[] args) {
         int k = 80;
         int[][] dungeons = {{80,20},{50,40},{30,10}};
-        int answer = -1;
 
         boolean[] visited = new boolean[dungeons.length];
         bfs(0, k, dungeons, visited);
